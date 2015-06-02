0ff0553e5bd5d04aff7af23aaa1c28402968e4c0
Xu <xujijun@126.com>
2015-06-02 14:28:21 +0800

    Juery, Log4j2.3

---
+++ src/main/webapp/testcases.js
@@ -0,0 +1,25 @@
+//测试用例列表：
+
+/*
+ * 按模块编号：
+ * 0、
+ * 
+ */
+var testCases = [
+	{
+		title : "0.1 用Map接收前端提交的Form Data或Query String",
+		url : "/mapParameter/map",
+		requestBody : "authCode=123456&accountName=15888888888&password=888962&password=666566"
+	},
+	{
+		title : "0.2 用MultiValueMap接收前端提交的Form Data或Query String",
+		url : "/mapParameter/multiValueMap",
+		requestBody : "authCode=123456&phone=15888888888&verifyType=0&verifyType=1"
+	},
+	{
+		title : "0.3 用Map接收前端提交的json格式的Request Payload",
+		url : "/mapParameter/jsonParams",
+		requestBody : '{"authCode":"123456","phone":"15888888888","code":0,"code":1}',
+		contentType : "application/json;charset=utf-8"
+	}
+];
