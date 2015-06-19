e5a3d104974c5343ab97c61addb2b811ab8d04b7
Xu <xujijun@126.com>
2015-06-19 19:03:19 +0800

    DB related config

---
+++ src/main/java/com/xjj/annotation/DbTest.java
@@ -0,0 +1,11 @@
+package com.xjj.annotation;
+
+/**
+ * db_ysbc_admin库数据源,applicationContext-mybatis.xml文件中annotationClass定义<br/>
+ * 使用方法:在DAO层interface中用@该数据源
+ * @author liriming
+ *
+ */
+public @interface DbTest {
+
+}
