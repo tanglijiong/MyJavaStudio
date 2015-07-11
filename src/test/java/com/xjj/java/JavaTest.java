7b259e4a937db71079f8411a35ef4a0b56cfe8d6
Xu <xujijun@126.com>
2015-07-11 13:48:00 +0800

    JavaTest

--- src/test/java/com/xjj/java/JavaTest.java
+++ src/test/java/com/xjj/java/JavaTest.java
@@ -4,11 +4,17 @@ import org.junit.Test;
 
 public class JavaTest {
 
+	boolean p(){
+		System.out.print("Hello ");
+		return false;
+	}
+	
 	@Test
 	public void HelloWorld(){
 	
 		//if(System.out.printf("Hello ")==null){
-		if(System.out.format("Hello ")==null){
+		//if(System.out.format("Hello ")==null){
+		if(p()){
 			System.out.print("Hello ");
 		}else {
 			System.out.println("World");
