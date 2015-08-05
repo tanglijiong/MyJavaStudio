a1b19d4916ae68351444dd0fc397827a3a46caa4
Xu <xujijun@126.com>
2015-08-05 09:58:38 +0800

    stock

--- src/main/java/com/xjj/http/HttpHelper.java
+++ src/main/java/com/xjj/http/HttpHelper.java
@@ -1,6 +1,8 @@
 package com.xjj.http;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -8,6 +10,8 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * 封装http请求的相关方法
@@ -17,15 +21,34 @@ import org.apache.http.util.EntityUtils;
  */
 public class HttpHelper {
 
+	public static Map<String, String> defaultHeaders = new HashMap<>();
+	static{
+		defaultHeaders.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+		defaultHeaders.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36");
+		defaultHeaders.put("Accept-Language", "zh-CN,zh;q=0.8");
+		defaultHeaders.put("Connection", "keep-alive");
+	};
+
+	public final static String[] userAgents = {"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0",
+		"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"};
+
+	
+	/**
+	 * 使用缺省的一些headers发送GET请求 
+	 * @param url
+	 * @return
+	 */
 	public static HttpResult doGet(String url) {
 		HttpResult result = new HttpResult();
 		HttpClient client = HttpClientBuilder.create().build();
-		HttpGet request = new HttpGet(url);
-		request.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-		request.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36");
-		request.setHeader("Accept-Language", "zh-CN,zh;q=0.8");
-		
+
 		try {
+			//HttpGet request = new HttpGet(URLEncoder.encode(url, "utf-8"));
+			HttpGet request = new HttpGet(url);
+			request.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+			request.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36");
+			request.setHeader("Accept-Language", "zh-CN,zh;q=0.8");
 			HttpResponse response = client.execute(request);
 			HttpEntity entity = response.getEntity();
 			result.setCode(response.getStatusLine().getStatusCode());
@@ -43,9 +66,56 @@ public class HttpHelper {
 
 		return result;
 	}
+	
+	/**
+	 * 使用指定的headers发送GET请求 
+	 * @param url
+	 * @param headers
+	 * @return
+	 */
+	public static HttpResult doGet(String url, Map<String, String> headers) {
+		HttpResult result = new HttpResult();
+		HttpClient client = HttpClientBuilder.create().build();
+		
+		try {
+			//HttpGet request = new HttpGet(URLEncoder.encode(url, "utf-8"));
+			HttpGet request = new HttpGet(url);
+			for(String key : headers.keySet()){
+				request.setHeader(key, headers.get(key));
+			}
+			HttpResponse response = client.execute(request);
+			HttpEntity entity = response.getEntity();
+			result.setCode(response.getStatusLine().getStatusCode());
+			if (entity != null) {
+				result.setMsg(EntityUtils.toString(entity, "UTF-8")); //解决中文乱码问题
+				//System.out.println(EntityUtils.getContentCharSet(entity));
+				//result.setMsg(EntityUtils.toString(entity));
+				if(result.getCode()!=200){
+					request.abort();
+				}
+			}
+		} catch (IOException e ) {
+			e.printStackTrace();
+		} finally {
+
+		}
+
+		return result;
+	}
 
 	public static void main(String[] args) {
-		HttpResult result = doGet("http://blog.csdn.net/clementad/article/details/46491701");
-		System.out.println(result.getMsg());
+		//HttpResult result = doGet("http://blog.csdn.net/clementad/article/details/46491701");
+		
+		Map<String, String> defaultHeaders = new HashMap<>();
+		defaultHeaders.put("apikey", "da47545cf2bf34e59a445eda060ee62b");
+		HttpResult result = doGet("http://apis.baidu.com/apistore/stockservice/stock?stockid=sz000002", defaultHeaders);
+		ObjectMapper jsonMapper = new ObjectMapper();
+		try {
+			@SuppressWarnings("unchecked")
+			Map<String, Object> r = jsonMapper.readValue(result.getMsg(), Map.class);
+			System.out.println(r);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/xjj/web/filter/NoCacheFilter.java
+++ src/main/java/com/xjj/web/filter/NoCacheFilter.java
@@ -12,29 +12,31 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * 防止浏览器缓存页面或请求结果
+ * 
  * @author XuJijun
  *
  */
 public class NoCacheFilter implements Filter {
 
 	@Override
-	public void destroy(){
+	public void destroy() {
 	}
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse)resp;
+		HttpServletResponse response = (HttpServletResponse) resp;
 
-        response.setDateHeader("Expires", -1);				//告诉浏览器数据可以缓存多长时间，-1或0表示不缓存
-        response.setHeader("Cache_Control", "no-cache");	//支持HTTP 1.1，告诉浏览器要不要缓存数据，如“no-cache”
-        response.setHeader("Pragma", "no-cache");			//支持HTTP 1.0，告诉浏览器要不要缓存数据，如“no-cache”
-        
-        response.setHeader("Access-Control-Allow-Origin", "*");     //允许跨域请求（CORS）
-        
-        chain.doFilter(req, resp);
+		response.setDateHeader("Expires", -1); // 告诉浏览器数据可以缓存多长时间，-1或0表示不缓存
+		response.setHeader("Cache_Control", "no-cache"); // 支持HTTP 1.1，告诉浏览器要不要缓存数据，如“no-cache”
+		response.setHeader("Pragma", "no-cache"); // 支持HTTP 1.0，告诉浏览器要不要缓存数据，如“no-cache”
+
+		response.setHeader("Access-Control-Allow-Origin", "*"); // 允许跨域请求（CORS）
+		response.setHeader("Access-Control-Allow-Headers", "*"); // 允许跨域请求的Headers（CORS）
+
+		chain.doFilter(req, resp);
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException	{
+	public void init(FilterConfig arg0) throws ServletException {
 	}
 }
